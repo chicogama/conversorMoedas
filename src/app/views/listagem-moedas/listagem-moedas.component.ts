7dd19361ca7e1200217650b598b9a4dd73fe9fee
chicogama <josemargama7@gmail.com>
2023-02-03 10:16:55 -0300

    deploy

--- src/app/views/listagem-moedas/listagem-moedas.component.ts
+++ src/app/views/listagem-moedas/listagem-moedas.component.ts
@@ -7,39 +7,39 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Moeda } from 'src/app/model/Moeda';
 
 @Component({
-  selector: 'app-listagem-moedas',
-  templateUrl: './listagem-moedas.component.html',
-  styleUrls: ['./listagem-moedas.component.css'],
+    selector: 'app-listagem-moedas',
+    templateUrl: './listagem-moedas.component.html',
+    styleUrls: ['./listagem-moedas.component.css'],
 })
 export class ListagemMoedasComponent implements OnInit {
-  @Input() moedas: Moeda[] = [];
-  readonly displayedColumns = ['code', 'description'];
-  dataSource = new MatTableDataSource<Moeda>(this.moedas);
+    @Input() moedas: Moeda[] = [];
+    readonly displayedColumns = ['code', 'description'];
+    dataSource = new MatTableDataSource<Moeda>(this.moedas);
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private moedaService: MoedaService) {}
+    constructor(private moedaService: MoedaService) {}
 
-  ngOnInit(): void {
-    try {
-      this.obterMoeda();
-    } catch (error) {
-      console.log('Ocorreu um erro inesperado ' + error);
+    ngOnInit(): void {
+        try {
+            this.obterMoeda();
+        } catch (error) {
+            console.log('Ocorreu um erro inesperado ' + error);
+        }
     }
-  }
 
-  obterMoeda() {
-    this.moedaService.listarMoedas().subscribe((resposta) => {
-      this.moedas = Object.values(resposta.symbols);
-      console.log(this.moedas);
-      this.dataSource = new MatTableDataSource<Moeda>(this.moedas);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    });
-  }
+    obterMoeda() {
+        this.moedaService.listarMoedas().subscribe((resposta) => {
+            this.moedas = Object.values(resposta.symbols);
+            console.log(this.moedas);
+            this.dataSource = new MatTableDataSource<Moeda>(this.moedas);
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+        });
+    }
 
-  filtrarMoedas($event: any) {
-    this.dataSource.filter = $event.target.value;
-  }
+    filtrarMoedas($event: any) {
+        this.dataSource.filter = $event.target.value;
+    }
 }
